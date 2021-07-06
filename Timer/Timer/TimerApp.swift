8770073c91ad089ed035d210ea65dfe05cc0597a
kazuya <kazuyasky@gmail.com>
2021-07-06 18:39:40 +0900

    create project

---
+++ Timer/Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by 小田和哉 on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
